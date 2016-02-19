51f46dc1247db546eaae85d808abeaf2df372b4f
pkflash <jamie.lee@gatech.edu>
2016-02-18 23:39:10 -0500

    finished a d2q9 example!

--- diffusion_d2q9/main.m
+++ diffusion_d2q9/main.m
@@ -27,32 +27,50 @@ omega = 1 / (3*alpha/(dt*csq)+0.5);
 % Initialize.
 x = linspace(0,L(1),nodes(1))';
 y = linspace(0,L(2),nodes(2))';
-rho = zeros(nodes(2),nodes(1));
-f = zeros(9,nodes(2),nodes(1));
+f = zeros(nodes(2),nodes(1),9);
 % BC initialize.
 for k = 1:9
-    f(k,:,1) = w(k)*twall; 
+    f(:,1,k) = w(k)*twall; % left wall.
 end
 
 % Main loop.
 for iter = 1:timesteps
+    % Reconstruct T.
+    rho = f(:,:,1);
+    for k = 2:9
+        rho = rho + f(:,:,k);
+    end
     % Collision
-    rho = sum(f,1);
-    feq = w*rho;
-    f = omega*feq + (1-omega)*f;
+    for k = 1:9
+        feq = w(k)*rho;
+        f(:,:,k) = omega*feq + (1-omega)*f(:,:,k);
+    end
     % Streaming
-    f(2,:,2:end) = f(2,:,1:end-1); % East vector.
-    f(3,2:end,:) = f(3,1:end-1,:); % North vector.
-    f(4,:,1:end-1) = f(4,:,2:end); % West vector.
-    f(5,1:end-1,:) = f(5,2:end,:); % South vector.
-    f(6,2:end,2:end) = f(6,1:end-1,1:end-1); % Northeast vector.
-    f(7,2:end,1:end-1) = f(7,1:end-1,2:end); % Northwest vector.
-    f(8,1:end-1,1:end-1) = f(8,2:end,2:end); % Southwest vector.
-    f(9,1:end-1,2:end) = f(9,2:end,1:end-1); % Southeast vector.
+    f(:,2:end,2) = f(:,1:end-1,2); % East vector.
+    f(2:end,:,3) = f(1:end-1,:,3); % North vector.
+    f(:,1:end-1,4) = f(:,2:end,4); % West vector.
+    f(1:end-1,:,5) = f(2:end,:,5); % South vector.
+    f(2:end,2:end,6) = f(1:end-1,1:end-1,6); % Northeast vector.
+    f(2:end,1:end-1,7) = f(1:end-1,2:end,7); % Northwest vector.
+    f(1:end-1,1:end-1,8) = f(2:end,2:end,8); % Southwest vector.
+    f(1:end-1,2:end,9) = f(2:end,1:end-1,9); % Southeast vector.
     % Boundary conditions
-    
-    % Compute solution
-    rho = f1+f2+f3+f4;
+    f(:,1,2) = w(2)*twall + w(4)*twall - f(:,1,4); % Left boundary, T = 1.0.
+    f(:,1,6) = w(6)*twall + w(8)*twall - f(:,1,8); % Left boundary, T = 1.0.
+    f(:,1,9) = w(9)*twall + w(7)*twall - f(:,1,7); % Left boundary, T = 1.0.
+    f(:,end,4) = -f(:,end,2); % Right boundary, T = 0.
+    f(:,end,7) = -f(:,end,9); % Right boundary, T = 0.
+    f(:,end,8) = -f(:,end,6); % Right boundary, T = 0.
+    f(end,:,5) = -f(end,:,3); % Top boundary, T = 0.
+    f(end,:,8) = -f(end,:,6); % Top boundary, T = 0.
+    f(end,:,9) = -f(end,:,7); % Top boundary, T = 0.
+    for k = 2:9
+        f(1,:,k) = f(2,:,k); % Bottom boundary, adiabatic.
+    end
+end
+rho = f(:,:,1);
+for k = 2:9
+    rho = rho + f(:,:,k);
 end
 
 % Plotting results!

5058eeff529e33f870992f44c456e6df3ffd9b70
pkflash <jamie.lee@gatech.edu>
2016-02-19 01:08:10 -0500

    finished advection-diffusion

--- advection_diffusion/d2q9.m
+++ advection_diffusion/d2q9.m
@@ -4,12 +4,18 @@ clear;close all;clc;
 % loop, compared to D1Q2 and D2Q4, which both have lattice links of equal
 % weight.
 
+% As you may have noticed, the addition of advection only changes the
+% collision step, and not the streaming step nor the BCs (but we did change 
+% the type of BC from the diffusion-only version).
+
 % Input parameters.
 nodes = [100, 100]; % x nodes, y nodes.
+u = 0.1;
+v = 0.4;
 dh = 1; % dh = dx = dy.
 dt = 1; % timestep.
 timesteps = 400;
-alpha = 0.25; % Physical constant.
+alpha = 1.0; % Physical constant.
 twall = 1.0; % Left-hand wall temperature.
 
 % Constants.
@@ -20,14 +26,15 @@ w(6:9) = 1/36;
 
 % Derived inputs.
 L = dh*[nodes(1)-1, nodes(2)-1] ; % x , y dimensions of physical domain.
-c = dh/dt;
-csq = c.^2;
+ck = dh/dt;
+csq = ck.^2;
 omega = 1 / (3*alpha/(dt*csq)+0.5);
 
 % Initialize.
 x = linspace(0,L(1),nodes(1))';
 y = linspace(0,L(2),nodes(2))';
 f = zeros(nodes(2),nodes(1),9);
+feq = zeros(nodes(2),nodes(1),9);
 % BC initialize.
 for k = 1:9
     f(:,1,k) = w(k)*twall; % left wall.
@@ -48,9 +55,17 @@ for iter = 1:timesteps
     reconstruction_time = reconstruction_time + toc;
     % Collision
     tic;
+    feq(:,:,1) = w(1)*rho;
+    feq(:,:,2) = w(2)*rho*(1 + 3*u/ck);
+    feq(:,:,3) = w(3)*rho*(1 + 3*v/ck);
+    feq(:,:,4) = w(4)*rho*(1 - 3*u/ck);
+    feq(:,:,5) = w(5)*rho*(1 - 3*v/ck);
+    feq(:,:,6) = w(6)*rho*(1 + 3*(u+v)/ck);
+    feq(:,:,7) = w(7)*rho*(1 + 3*(-u+v)/ck);
+    feq(:,:,8) = w(8)*rho*(1 - 3*(u+v)/ck);
+    feq(:,:,9) = w(9)*rho*(1 + 3*(u-v)/ck);
     for k = 1:9
-        feq = w(k)*rho;
-        f(:,:,k) = omega*feq + (1-omega)*f(:,:,k);
+        f(:,:,k) = omega*feq(:,:,k) + (1-omega)*f(:,:,k);
     end
     collision_time = collision_time + toc;
     % Streaming
@@ -72,12 +87,21 @@ for iter = 1:timesteps
     f(:,end,4) = -f(:,end,2); % Right boundary, T = 0.
     f(:,end,7) = -f(:,end,9); % Right boundary, T = 0.
     f(:,end,8) = -f(:,end,6); % Right boundary, T = 0.
+    f(:,end,3) = -f(:,end,5); % Right boundary, T = 0.
+    f(:,end,1) = 0; % Right boundary, T = 0.
     f(end,:,5) = -f(end,:,3); % Top boundary, T = 0.
     f(end,:,8) = -f(end,:,6); % Top boundary, T = 0.
     f(end,:,9) = -f(end,:,7); % Top boundary, T = 0.
-    for k = 2:9
-        f(1,:,k) = f(2,:,k); % Bottom boundary, adiabatic.
-    end
+    f(end,:,2) = -f(end,:,4); % Top boundary, T = 0.
+    f(end,:,1) = 0; % Top boundary, T = 0.
+%     for k = 2:9
+%         f(1,:,k) = f(2,:,k); % Bottom boundary, adiabatic.
+%     end
+    f(1,:,3) = -f(1,:,5); % Bottom boundary, T = 0.
+    f(1,:,7) = -f(1,:,9); % Bottom boundary, T = 0.
+    f(1,:,6) = -f(1,:,8); % Bottom boundary, T = 0.
+    f(1,:,2) = -f(1,:,4); % Bottom boundary, T = 0.
+    f(1,:,1) = 0; % Bottom boundary, T = 0.
     bc_time = bc_time + toc;
 end
 tic;
